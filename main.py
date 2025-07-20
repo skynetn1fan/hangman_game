53777a06e77ceafceea14fa2b139857a0ba8240f
root <root@181fe40abc79>
2025-07-20 13:20:29 +0000

    Add or update docstrings on 2025-07-20 13:20:29

--- main.py
+++ main.py
@@ -13,7 +13,19 @@ time.sleep(1)
 
 
 def get_valid_word():
-    with open('wordlist.txt') as f:
+    """Get a valid word from a word list file.
+
+    Reads a list of words from 'wordlist.txt', removes any trailing newline characters,
+    and selects a random word from the list, converting it to uppercase.
+
+    Returns:
+        str: A randomly selected uppercase word from the word list.
+
+    Raises:
+        FileNotFoundError: If 'wordlist.txt' does not exist.
+        IOError: If there is an error reading the file.
+    """
+    with open("wordlist.txt") as f:
         words_to_guess = f.readlines()
     for i in range(len(words_to_guess)):
         words_to_guess[i] = words_to_guess[i][0:-1]
@@ -22,7 +34,16 @@ def get_valid_word():
 
 
 def play_game():
-    play_game = input('Again? y or n')
+    """Prompt the user to decide whether to play the game again.
+
+    Continuously prompts the user until a valid input ('y' or 'n') is received.
+    If 'y', the hangman game is restarted. If 'n', a farewell message is printed
+    and the program exits.
+
+    Returns:
+        None
+    """
+    play_game = input("Again? y or n")
     while play_game not in ["y", "n", "Y", "N"]:
         play_game = input("Do You want to play again? y = yes, n = no \n")
     if play_game == "y":
@@ -31,8 +52,23 @@ def play_game():
         print("Thanks For Playing! We expect you back again!")
         exit()
 
+
 # Initializing all the conditions required for the game:
+
+
 def hangman():
+    """Play a game of Hangman.
+
+    Initializes the game with a random word and allows the user to guess letters
+    until they either guess the word correctly or run out of lives. The game
+    displays the current state of the word and the letters used so far.
+
+    Returns:
+        None
+
+    Raises:
+        ValueError: If the user input is invalid (e.g., more than one character or a number).
+    """
     limit = 5
     word = get_valid_word()
     word_letters = set(word)
@@ -40,12 +76,14 @@ def hangman():
     used_letters = set()
 
     length = len(word)
-    display = '_ ' * length
+    display = "_ " * length
     letters_used = set()
 
     while limit > 0 and len(word_letters) > 0:
 
-        print(f'You have used the following letters: {letters_used} and have {limit} lives left')
+        print(
+            f"You have used the following letters: {letters_used} and have {limit} lives left"
+        )
         guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
         guess = guess.strip().upper()
         letters_used.add(guess)
@@ -59,15 +97,16 @@ def hangman():
                 display[place] = guess
                 word_letters.remove(guess)
             else:
-                print('You have already tried this one')
+                print("You have already tried this one")
         else:
             limit -= 1
-            print('That letter is not in the word, try again.')
+            print("That letter is not in the word, try again.")
 
-    if limit==0:
-        print('Sorry you ran out of lives, it\'s over')
-    elif len(word_letters) ==0:
-        print('Yaey, you did it!')
+    if limit == 0:
+        print("Sorry you ran out of lives, it's over")
+    elif len(word_letters) == 0:
+        print("Yaey, you did it!")
         play_game()
+
 
 hangman()
